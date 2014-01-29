8251c480408ddd730186659820fb207e3100d31d
Taurustar <scarreno@dark-ware.com>
2014-01-28 22:22:54 -0400

    Terminado
    la app terminada

--- Version1/Version1/MainPage.xaml.cs
+++ Version1/Version1/MainPage.xaml.cs
@@ -17,14 +17,83 @@ namespace Version1
         public MainPage()
         {
             InitializeComponent();
+            btnFlipTile_Click();
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
         }
 
-        private void ss(object sender, System.Windows.Input.GestureEventArgs e)
+
+        private void btnFlipTile_Click()
         {
-            NavigationService.Navigate(new Uri("/Page1.xaml", UriKind.Relative));
+            // find the tile object for the application tile that using "flip" contains string in it.
+            ShellTile oTile = ShellTile.ActiveTiles.FirstOrDefault(x => x.NavigationUri.ToString().Contains("flip".ToString()));
+
+
+            if (oTile != null && oTile.NavigationUri.ToString().Contains("flip"))
+            {
+                FlipTileData oFliptile = new FlipTileData();
+                oFliptile.Title = "";
+                oFliptile.Count = 0;
+                oFliptile.BackTitle = "YAMI";
+
+                oFliptile.BackContent = "";
+                oFliptile.WideBackContent = "Elige tus Frutas Frescas";
+
+                oFliptile.SmallBackgroundImage = new Uri("Assets/LosTiles/chico.png", UriKind.Relative);
+                oFliptile.BackgroundImage = new Uri("Assets/LosTiles/medio3.png", UriKind.Relative);
+                oFliptile.WideBackgroundImage = new Uri("Assets/LosTiles/grande.png", UriKind.Relative);
+
+                oFliptile.BackBackgroundImage = new Uri("/Assets/LosTiles/medio2.png", UriKind.Relative);
+                oFliptile.WideBackBackgroundImage = new Uri("/Assets/LosTiles/grande2.png", UriKind.Relative);
+
+                //oFliptile.BackBackgroundImage = new Uri("/Assets/LosTiles/medio1.png", UriKind.Relative);
+                oTile.Update(oFliptile);
+                //MessageBox.Show("Flip Tile Data successfully update.");
+            }
+            else
+            {
+                // once it is created flip tile
+                Uri tileUri = new Uri("/MainPage.xaml?tile=flip", UriKind.Relative);
+                ShellTileData tileData = this.CreateFlipTileData();
+                ShellTile.Create(tileUri, tileData, true);
+            }
+        }
+
+        private ShellTileData CreateFlipTileData()
+        {
+            return new FlipTileData()
+            {
+                Title = "YAMI",
+                BackTitle = "frutas ricas",
+                BackContent = "Live Tile Demo",
+                WideBackContent = "Hello Nokia Lumia 920",
+                Count = 8,
+                SmallBackgroundImage = new Uri("/Assets/Tiles/FlipCycleTileSmall.png", UriKind.Relative),
+                BackgroundImage = new Uri("/Assets/Tiles/FlipCycleTileMedium.png", UriKind.Relative),
+                WideBackgroundImage = new Uri("/Assets/Tiles/FlipCycleTileLarge.png", UriKind.Relative),
+            };
+        }
+
+
+        private void irATomate(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Tomate.xaml", UriKind.Relative));
+        }
+
+        private void irAMelon(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Melon.xaml", UriKind.Relative));
+        }
+
+        private void irASandia(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Sandia.xaml", UriKind.Relative));
+        }
+
+        private void irAPalta(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Palta.xaml", UriKind.Relative));
         }
 
         // Sample code for building a localized ApplicationBar
